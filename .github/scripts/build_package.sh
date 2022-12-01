4ab5781a4103ee931a7baea2b0051bc57f6ec2d1
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-12-01 15:02:55 -0500

    Change Rscript file piping

--- .github/scripts/build_package.sh
+++ .github/scripts/build_package.sh
@@ -5,7 +5,6 @@ LIBRARY=$1
 PKG=$2
 mkdir -p $LIBRARY
 mkdir -p /tmp/tars/
-mkdir -p /tmp/logs/
 sed -n "/^    \"$PKG\"/,/^    \"/p" original.json | grep '^        "' | awk -F'"' '{print $2}' > /tmp/deps
 if [ -s /tmp/deps ]; then cat /tmp/deps | xargs -i cat lists/{} | xargs -i bash -c "curl -o $LIBRARY/{} https://js2.jetstream-cloud.org:8001/swift/v1/gha-build/container/{} && ( cd $LIBRARY && tar -xvf {} && rm {} )"; else echo "No dependencies"; fi
 CLONED=0
@@ -16,13 +15,13 @@ then sleep 5 && if ! git clone https://git.bioconductor.org/packages/$PKG
             then sleep 5 && if ! git clone https://git.bioconductor.org/packages/$PKG
                 then
                     CLONED=1
-                    Rscript -e "Sys.setenv(BIOCONDUCTOR_USE_CONTAINER_REPOSITORY=FALSE); p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('$PKG', INSTALL_opts = '--build', update = TRUE, quiet = FALSE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)" 2>&1 > /tmp/$PKG
+                    Rscript -e "Sys.setenv(BIOCONDUCTOR_USE_CONTAINER_REPOSITORY=FALSE); p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('$PKG', INSTALL_opts = '--build', update = TRUE, quiet = FALSE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)" > /tmp/$PKG 2>&1
                 fi
             fi
         fi
     fi
 fi
-if [ $CLONED = 0 ]; then Rscript -e "p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); install.packages('$(pwd)/$PKG', INSTALL_opts = '--build', update = TRUE, quiet = FALSE, force = TRUE, keep_outputs = TRUE, type = 'source', repos = NULL); if('$PKG' %in% rownames(installed.packages())) q(status = 0) else q(status = 1)"; fi 2>&1 > /tmp/$PKG
+if [ $CLONED = 0 ]; then Rscript -e "p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); install.packages('$(pwd)/$PKG', INSTALL_opts = '--build', update = TRUE, quiet = FALSE, force = TRUE, keep_outputs = TRUE, type = 'source', repos = NULL); if('$PKG' %in% rownames(installed.packages())) q(status = 0) else q(status = 1)"; fi > /tmp/$PKG 2>&1 
 
 cat /tmp/$PKG
 cp *.tar.gz /tmp/tars/
