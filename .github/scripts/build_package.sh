1dfe5f6c9a2cd8bd2f99ef7c424fce2101ae84b8
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-12-21 01:36:43 -0500

    Fix pull

--- .github/scripts/build_package.sh
+++ .github/scripts/build_package.sh
@@ -9,7 +9,7 @@ mkdir -p /tmp/tars/
 
 # Get direct dependency list to pull their libraries from their build run
 sed -n "/^    \"$PKG\"/,/^    \"/p" directdeps.json | grep '^        "' | awk -F'"' '{print $2}' > /tmp/deps
-if [ -s /tmp/deps ]; then cat /tmp/deps | xargs -I## bash -c 'if [ -d $LIBRARY/## ]; then echo "Dependency ## is already found."; else cat lists/## | xargs -i bash -c "curl -o $LIBRARY/{} https://js2.jetstream-cloud.org:8001/swift/v1/gha-build/running/$runstart/{} && ( cd $LIBRARY && tar -xvf {} && rm {} )"; fi'; else echo "No dependencies"; fi
+if [ -s /tmp/deps ]; then cat /tmp/deps | xargs -I## bash -c "if [ -d $LIBRARY/## ]; then echo 'Dependency ## is already found.'; else cat lists/## | xargs -i bash -c 'curl -o $LIBRARY/{} https://js2.jetstream-cloud.org:8001/swift/v1/gha-build/running/$runstart/{} && ( cd $LIBRARY && tar -xvf {} && rm {} )'; fi"; else echo "No dependencies"; fi
 
 Rscript -e "Sys.setenv(BIOCONDUCTOR_USE_CONTAINER_REPOSITORY=FALSE); p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('$PKG', INSTALL_opts = '--build', update = TRUE, quiet = FALSE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)" > /tmp/$PKG 2>&1
 
