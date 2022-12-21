7fa94599410c9b4dc27c482e55a5b1a86cee073c
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-12-21 14:33:34 -0500

    Fixes

--- .github/scripts/download_artifact.sh
+++ .github/scripts/download_artifact.sh
@@ -5,7 +5,6 @@ GITREPO=${3:-"almahmoud/gha-build"}
 
 RUNID=$(cat logs/run_ids/rstudio-binaries/$DEPNAME | grep "runs/" | head -n 1 | awk -F'runs/' '{print $2}' | awk -F'/' '{print $1}')
 ARTIFACT_ID=$(gh api -H "Accept: application/vnd.github+json" /repos/$GITREPO/actions/artifacts?name="artifact-$DEPNAME" --jq ".artifacts[] | select(.workflow_run.id==$RUNID) | .id")
-TARNAME="$(cat lists/$DEPNAME)"
 
 echo $RUNID
 echo $ARTIFACT_ID
@@ -15,6 +14,7 @@ curl -o $DEPNAME.zip -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/almahmoud/gha-build/actions/artifacts/485165954/zip
   https://api.github.com/repos/$GITREPO/actions/artifacts/$ARTIFACT_ID/zip &&\
 unzip $DEPNAME.zip && rm $DEPNAME.zip &&\
 tar -xvf "$DEPNAME".tar.gz && rm "$DEPNAME".tar.gz )
