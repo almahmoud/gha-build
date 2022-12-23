1426e7837ba33f0ff69a08702b25d4a9d7f3f224
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-12-23 02:04:19 -0500

    Fix

--- .github/scripts/build_package.sh
+++ .github/scripts/build_package.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
 set -xe
 git pull origin main || git reset --hard origin/main
-LIBRARY=$1
-PKG=$2
-runstart=$3
+export LIBRARY=$1
+export PKG=$2
+export runstart=$3
 mkdir -p $LIBRARY
 mkdir -p /tmp/tars/
 
 # Get direct dependency list to pull their libraries from their build run
 sed -n "/^    \"$PKG\"/,/^    \"/p" directdeps.json | grep '^        "' | awk -F'"' '{print $2}' > /tmp/deps
 
-function check_dep_present() {
+function process_dep() {
   if [ -d $LIBRARY/$1 ]; then
     echo "Dependency $1 is already found."
-    return 0
-  else
-    return 1
-  fi
-}
-
-function download_and_extract_dep() {
-  bash -c "curl -o $LIBRARY/$1.tar.gz https://js2.jetstream-cloud.org:8001/swift/v1/gha-build/running/$runstart/$1.tar.gz && ( cd $LIBRARY && tar -xvf $1.tar.gz && rm $1.tar.gz )"
-}
-
-function process_dep() {
-  if check_dep_present "$1"; then
     return
   else
-    download_and_extract_dep "$1"
+  	bash -c "curl -o $LIBRARY/$1.tar.gz https://js2.jetstream-cloud.org:8001/swift/v1/gha-build/running/$runstart/$1.tar.gz && ( cd $LIBRARY && tar -xvf $1.tar.gz && rm $1.tar.gz )"
   fi
 }
 
+export -f process_dep
+
 function process_deps() {
-  cat /tmp/deps | xargs -i process_dep {}
+  cat /tmp/deps | xargs -i bash -c "process_dep {}"
 }
 
 if [ -s /tmp/deps ]; then
