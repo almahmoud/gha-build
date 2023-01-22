465f70fbf5e5caf4a7128968047b858dedfe5ae5
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-01-21 19:07:17 -0500

    ls

--- .github/scripts/output_versions.sh
+++ .github/scripts/output_versions.sh
@@ -2,6 +2,6 @@
 echo os=$(Rscript -e 'sessionInfo()$running') >> $GITHUB_OUTPUT
 echo r=$(Rscript -e 'R.Version()$version.string') >> $GITHUB_OUTPUT
 echo bioc=$(Rscript -e 'BiocManager::version()') >> $GITHUB_OUTPUT
-echo library=$(echo "/tmp/built/") >> $GITHUB_OUTPUT
+echo library=$(echo "tmp/built") >> $GITHUB_OUTPUT
 echo platform=$(cat arch) >> $GITHUB_OUTPUT
 echo sanitizedarch=$(cat arch | sed "s/[^[:alnum:]]/-/g") >> $GITHUB_OUTPUT
