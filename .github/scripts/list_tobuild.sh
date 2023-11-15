09558b712c3df9767b99d9da911f17ead24f6b78
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-11-15 12:49:30 -0500

    Changes for switching to self-hosted runner after failures

--- .github/scripts/list_tobuild.sh
+++ .github/scripts/list_tobuild.sh
@@ -25,6 +25,10 @@ if [ ! -s tobuild.txt ]; then
                 grep -Ervl "(failed|tar.gz$)" lists | grep -v "failed" > /tmp/resetpkgs || true
                 if [ -s /tmp/resetpkgs ]; then
                     mkdir -p logs/$(cat runstarttime)/retries
+                    if [ $counter -gt 6 ]; then
+                        echo 'switch' > lists/self-hosted
+                        rm -rf "logs/$(cat runstarttime)/retries" || true
+                    fi
                     cat /tmp/resetpkgs | xargs -i bash -c 'pkgcounter=0; retrypath="logs/$(cat runstarttime)/retries/{}"; if [ -f "$retrypath" ]; then pkgcounter=$(<$retrypath); fi; pkgcounter=$((pkgcounter+1)); mkdir -p $(dirname $retrypath); echo $pkgcounter > "$retrypath"; if [ $pkgcounter -lt 3 ]; then rm {} && rm logs/$(cat runstarttime)/retries_counter || true; fi'
                     git add lists
                     git add logs
