3fb8cf715b2491f248c408f8e97423a86bf59529
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-11-16 21:26:29 -0500

    Use /tmp for self-hosted flag

--- .github/scripts/list_tobuild.sh
+++ .github/scripts/list_tobuild.sh
@@ -26,7 +26,7 @@ if [ ! -s tobuild.txt ]; then
                 if [ -s /tmp/resetpkgs ]; then
                     mkdir -p logs/$(cat runstarttime)/retries
                     if [ $counter -gt 6 ]; then
-                        echo 'switch' > lists/self-hosted
+                        echo 'switch' > /tmp/self-hosted
                         rm -rf "logs/$(cat runstarttime)/retries" || true
                     fi
                     cat /tmp/resetpkgs | xargs -i bash -c 'pkgcounter=0; retrypath="logs/$(cat runstarttime)/retries/{}"; if [ -f "$retrypath" ]; then pkgcounter=$(<$retrypath); fi; pkgcounter=$((pkgcounter+1)); mkdir -p $(dirname $retrypath); echo $pkgcounter > "$retrypath"; if [ $pkgcounter -lt 3 ]; then rm {} && rm logs/$(cat runstarttime)/retries_counter || true; fi'
